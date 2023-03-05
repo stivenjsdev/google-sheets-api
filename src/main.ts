7b28e68f966be6eb8a2eab4367eea24f62418f9e
Stiven Trujillo <strujillodev@gmail.com>
2023-03-04 19:10:32 -0500

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -10,10 +10,8 @@ async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
-    {
-      cors: true
-    }
   );
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000, '0.0.0.0');
 }
