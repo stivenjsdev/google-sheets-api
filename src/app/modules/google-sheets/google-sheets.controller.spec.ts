380cf2928bd8aec7c60ccb7cd8b63c2244b68aa4
Stiven Trujillo <strujillodev@gmail.com>
2023-03-06 01:11:57 -0500

    config unit tests to gs controller

--- src/app/modules/google-sheets/google-sheets.controller.spec.ts
+++ src/app/modules/google-sheets/google-sheets.controller.spec.ts
@@ -1,18 +1,63 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
+import googleConfig from '../../../config/google.config';
 import { GoogleSheetsController } from './google-sheets.controller';
+import { GoogleSheetsService } from './google-sheets.service';
 
 describe('GoogleSheetsController', () => {
   let controller: GoogleSheetsController;
+  let service: GoogleSheetsService;
+  const spreadSheetId = 'spreadSheetId';
+  const sheetName = 'sheetName';
+  const range = 'range';
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot({
+          load: [googleConfig],
+        }),
+      ],
       controllers: [GoogleSheetsController],
+      providers: [
+        GoogleSheetsService,
+        {
+          provide: ConfigService,
+          useValue: {
+            get: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
+    service = module.get<GoogleSheetsService>(GoogleSheetsService);
     controller = module.get<GoogleSheetsController>(GoogleSheetsController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('An array of two empty arrays, should return an array with a single object', async () => {
+    const result = [[], []];
+    jest
+      .spyOn(service, 'findAll')
+      .mockImplementation(() => Promise.resolve(result));
+      
+    expect(await controller.getAll(spreadSheetId, sheetName, range)).toEqual([
+      {},
+    ]);
+  });
+
+  it('An array of three empty arrays, should return an array with two objects', async () => {
+    const result = [[], [], []];
+    jest
+      .spyOn(service, 'findAll')
+      .mockImplementation(() => Promise.resolve(result));
+
+    expect(await controller.getAll(spreadSheetId, sheetName, range)).toEqual([
+      {},
+      {},
+    ]);
   });
 });
