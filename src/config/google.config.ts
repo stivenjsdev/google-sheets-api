af5da5e31f69d13b674e0c499cca9c2b821eab53
Stiven Trujillo <strujillodev@gmail.com>
2023-03-12 01:50:08 -0500

    fix private key env var

--- src/config/google.config.ts
+++ src/config/google.config.ts
@@ -2,7 +2,7 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('google', () => ({
   google: {
-    privateKey: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+    privateKey: process.env.PRIVATE_KEY?.replace(/\\n/g, '\n'),
     clientEmail: process.env.CLIENT_EMAIL,
   },
 }));
